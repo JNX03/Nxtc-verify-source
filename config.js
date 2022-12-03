bed2a8a588d8d70d3e02d23cf925b1d70ee39bc6
JN03x <112270477+JNDEV03@users.noreply.github.com>
2022-12-03 15:57:30 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const config = {
+    // Discord Token
+    token: "your discord bot token",
+    command: "?createautorole",
+
+    // Embed Settings
+    embed: {
+        color: "#5865F2",
+        footer: "source code of nexttectzx bot - dev : jn03"
+    },
+
+    // Perms Can change the perms if wish
+    perms: "MANAGE_ROLES"
+}
+
+module.exports = config
