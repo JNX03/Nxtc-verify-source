9e5589654309f7e3bedf5e39d247940f684147d6
JN03x <112270477+JNDEV03@users.noreply.github.com>
2022-12-12 09:59:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,12 +6,13 @@ const config = {
 
     // Embed Settings
     embed: {
-        color: "#5865F2",
-        footer: "source code of nexttectzx bot - dev : jn03"
+        color: "#5865F2", //https://htmlcolorcodes.com/color-picker/
+        footer: "source code of nexttectzx bot - dev : jn03" //Btw you can change but pls give me an credit :D
     },
 
     // Perms Can change the perms if wish
-    perms: "MANAGE_ROLES"
+    perms: "MANAGE_ROLES" 
+    //Dont change anything
 }
 
 module.exports = config
