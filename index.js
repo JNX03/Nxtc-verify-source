a8ef9482dba25829584341dca1e9214c1f74ddd1
JN03x <112270477+JNDEV03@users.noreply.github.com>
2022-12-24 23:06:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ VS.on('messageCreate', async(message) => {
                 let mainembed = new MessageEmbed()
                     .setColor(config.embed.color)
                     .setDescription(msg)
-                  .setImage('You banner imaeg url')
+                  .setImage('You banner imaeg url') // Image url here dont ask me again .0.
                     .setFooter(config.embed.footer)
                 let row = new MessageActionRow()
                     .addComponents(
